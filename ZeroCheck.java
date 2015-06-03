5e69d07536e185e7d39dfd192c8ee7539a6be47a
jiaming <jli53@ncsu.edu>
2015-06-03 10:23:38 -0400

    1.7

--- ZeroCheck.java
+++ ZeroCheck.java
@@ -2,23 +2,22 @@ public class ZeroCheck{
 	public void check(int[][] init){
 		int row = init.length;
 		int column = init[0].length;
-		boolean zero = false;
+		boolean zero_row[] = new boolean[row];
+		boolean zero_column[] = new boolean[column];
 		for(int i = 0; i< row; i++)
 			for(int j = 0; j<column; j++){
 				if(init[i][j] == 0){
-					zero = true;
-					break;
+					zero_row[i] = true;
+					zero_column[j] = true;
 				}
 			}
-		if(zero == true){
-			for(int i = 0; i< row; i++){
-				java.util.Arrays.fill(init[i],0);
-				System.out.println(java.util.Arrays.toString(init[i]));
+		for(int i = 0; i < row; i++){
+			for(int j = 0; j < column; j++){
+				if(zero_row[i] == true || zero_column[j] == true){
+					init[i][j] = 0;
+				}
 			}
+			System.out.println(java.util.Arrays.toString(init[i]));
 		}
-		else
-			for(int i = 0; i < row; i++){
-				System.out.println(java.util.Arrays.toString(init[i]));
-			}
 	}
 }
